8cff6a502c569175a660c78cd98bb545e235109e
Michael Stewart <michaelstewart@Michaels-MBP.lan>
2024-03-07 10:29:36 -0800

    new additions

--- server/server.js
+++ server/server.js
@@ -2,12 +2,27 @@ const express = require('express')
 const app = express()
 const puppeteer = require('puppeteer')
 
-const getInfo = async () => {
+async function getInfo() {
     const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.drugs.com/history/fasenra.html');
+    // const body = await page.evaluate(() => 
+    //     document.body
+    // )
+    // console.log(body)
+    const content = await page.content()
+    const text = await page.evaluate(() => document.body.innerText)
+    console.log(text)
+
+
 }
+
 
 
 
 
 app.listen(5500, 
     console.log('app running'))
+
+
+getInfo()
